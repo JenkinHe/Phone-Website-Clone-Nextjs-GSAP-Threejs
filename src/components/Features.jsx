e26e5da5ec36ce908015293233e3f1ad48ef39fd
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-07-19 18:12:32 +1000

    text and animation preperation

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,10 +1,11 @@
 import { useGSAP } from '@gsap/react'
 import React from 'react'
+import { animateWithGsap } from '../utils/animations'
 
 const Features = () => {
 
     useGSAP(()=>{
-        
+        animateWithGsap('#features_title',{y:0, opacity:1})
     },[])
 
 
@@ -13,6 +14,15 @@ const Features = () => {
         <div className='screen-max-width'>
             <div className='mb-12 w-full'>
                 <h1 id='feature_title' className='section-heading'>Explore the full story</h1>
+            </div>
+
+            <div className='flex flex-col justify-center items-center overflow-hidden'>
+                <div className='mt-32 mb-24 pl-24'>
+                    <h2 className='text-5xl lg:text-7xl font-semibold'>iPhone.</h2>
+                    <h2 className='text-5xl lg:text-7xl font-semibold'>Forged in titanium.</h2>
+
+                </div>
+
             </div>
         </div>
     </section>
