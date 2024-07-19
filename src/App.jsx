b97e7b8f19d97ac096636e93ee7679f159b9497d
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-07-19 19:31:38 +1000

    simple footer complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Features from "./components/Features";
 import HowItWorks from './components/HowItWorks'
+import Footer from "./components/Footer";
 
 
 const App=() =>{
@@ -18,6 +19,7 @@ const App=() =>{
         <Model/>
         <Features/>
         <HowItWorks/>
+        <Footer/>
       </main>
     </>
   )
